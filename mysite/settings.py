c5919d7c76d233576d97a3b4c964c393c9887c00
Hane Liu <hane0131@gmail.com>
2017-06-06 14:32:10 +0800

    debug mode to false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yi*^j8r4q^4vj(^-f8(g7&v57lm46&lj_48it&yunpz4j&5t-i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'ooad-django-project.herokuapp.com']
 
